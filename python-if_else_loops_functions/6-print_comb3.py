e31364e4ef563a4590c599732dc7e0ca89cdc64e
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-01-19 17:07:55 -0400

    Mssing blank space after the dilimiter ',' in the print

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for digit1 in range(0, 10):
     for digit2 in range(digit1 + 1, 10):
         if digit1 != 8:
-            print('{}{},'.format((digit1), (digit2)), end='')
+            print('{}{}, '.format((digit1), (digit2)), end='')
         else:
             print('{}{}'.format((digit1), (digit2)))
