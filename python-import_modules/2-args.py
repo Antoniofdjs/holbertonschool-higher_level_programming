b84e238b44d43cb4090a3d62922c8f24468b84b6
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-01-22 11:38:28 -0400

    Fixed indentation

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-import sys
-# python3 --version 
 if __name__ == "__main__":
-    argc = len(sys.argv)
-    count = 1
-
-    if argc == 1:
-        print("0 arguments.")
-    elif argc == 2:
-        print("1 argument:")
-        print("1: {}".format(str(sys.argv[1])))
-    else:
-        print("{} arguments:".format(argc - 1))
-        for i in range(1, argc):
-            print("{}: {}".format(i, str(sys.argv[i])))
+    import sys
+argc = len(sys.argv)
+count = 1
+if argc == 1:
+    print("{} arguments.".format(argc - 1))
+elif argc == 2:
+    print("{} argument:".format(count))
+    print("{}: {}".format(count, str(sys.argv[count])))
+else:
+    print("{} arguments:".format(argc - 1))
+    my_arg = 1
+    while my_arg < argc:
+        print("{}: {}".format(my_arg, str(sys.argv[my_arg])))
+        my_arg += 1
