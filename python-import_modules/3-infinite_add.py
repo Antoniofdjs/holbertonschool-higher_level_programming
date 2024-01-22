9568bd73aba11dc29ec45d5b91eb5eedf2bd7f1a
Antoniofdjs <Antoniofdjs@gmail.com>
2024-01-22 11:52:55 -0400

    added permissions and delted white spaces after range

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
 argc = len(sys.argv)
 total = 0
-for i in range (1, argc):
+for i in range(1, argc):
     total += int(sys.argv[i])
 print('{}'.format(total))
