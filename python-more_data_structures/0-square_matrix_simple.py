69d9d31c6891b93a67800874f85b9199c4620661
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-01-29 11:18:17 -0400

    if name == main added

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = [row[:] for row in matrix]
-    new_matrix = list(map(lambda row: list(map(lambda x: x**2, row)),matrix))
-    return new_matrix
+if __name__ == "__main__":
+    def square_matrix_simple(matrix=[]):
+        new_matrix = [row[:] for row in matrix]
+        new_matrix = list(map(lambda row: list(map(lambda x: x**2, row)),matrix))
+        return new_matrix
