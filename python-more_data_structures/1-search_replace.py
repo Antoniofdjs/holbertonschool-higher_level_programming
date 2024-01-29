8fe6a82e95c042fc5878bf35f40b606af50e348f
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-01-29 12:17:09 -0400

    using map and lambda

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for i in range(0, len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
+    new_list = list(map(lambda x: replace if search == x else x, my_list))
     return new_list
