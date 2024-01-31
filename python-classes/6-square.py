3d6c4467b36f616a599f2fe7eecc11c6b355fc9f
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-01-31 17:37:05 -0400

    added a print() for position[1]

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -65,7 +65,4 @@ class Square:
             for _ in range(self.__position[1]):
                 print()
             for _ in range(0, self.__size):
-                if self.__position[1] > 0 or self.position[0] == 0:
-                    print('#' * self.__size)
-                else:
-                    print(' ' * self.__position[0] + '#' * self.__size)
+                print(' ' * self.__position[0] + '#' * self.__size)
