2268bf9c98a9080dc9927cc84514e4466218c518
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-01-31 17:02:09 -0400

    change- order of raise, now it checks first data types for the elements of tuple

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -19,9 +19,9 @@ class Square:
         # The position related block
         if type(position) is not tuple or len(position) < 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if position[0] < 0 or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
         if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
@@ -51,9 +51,9 @@ class Square:
     def position(self, value):
         if type(value) is not tuple or len(value) < 2:
             raise TypeError("value must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
