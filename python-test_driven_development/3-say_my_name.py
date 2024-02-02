e12a9816425c43a0024b0300a76633b1042d85d8
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-01 22:40:10 -0400

    optional check, if not revert

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -14,10 +14,6 @@ def say_my_name(first_name, last_name=""):
     error_a = "first_name must be a string"
     error_b = "last_name must be a string"
 
-    if first_name is None:
-        raise TypeError(error_a)
-    if last_name is None:
-        raise TypeError(error_b)
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
