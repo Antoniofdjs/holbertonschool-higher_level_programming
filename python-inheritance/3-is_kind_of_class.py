68d10a5e9dd9737c2d65e1b2376a9840cc45a802
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-07 22:15:50 -0400

    chmod

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,10 +2,6 @@
 """Returns true or false if obj is a_class, inheritance considered"""
 
 
-#!/usr/bin/python3
-"""Returns true or false if obj is a_class"""
-
-
 def is_same_class(obj, a_class):
     """
     Write a function that returns True if the object is exactly an
