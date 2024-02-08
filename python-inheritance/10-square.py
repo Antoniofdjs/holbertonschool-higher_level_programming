3f829f714a86d08fb8c44b9ecb33d6d9e565a0f0
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-08 14:28:58 -0400

    white spaces deleted

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -15,8 +15,8 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
 
-    def area (self):
+    def area(self):
         return self.__size ** 2
-    
+
     def __str__(self):
         return "[Square] {}/{}".format(self.__size, self.__size)
