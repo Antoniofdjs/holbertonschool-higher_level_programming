c601c24f58d4568c8349e18e393a37347a642111
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-07 23:54:10 -0400

    Change- now its all in the return

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,7 +4,4 @@
 
 def inherits_from(obj, a_class):
     """Check if obj is an instance of a class that inherited from a_class."""
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    else:
-        return False
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
