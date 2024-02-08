bb9dda45a3fbad59d3150c936080c8e303394c17
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-07 22:20:24 -0400

    another test, checcekr not passing

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""Returns true or false if obj is a_class"""
+"""
+Write a function that returns True if the object is an instance
+of, or if the object is an instance of a class that inherited
+from, the specified class ; otherwise False
+"""
 
 
-def is_same_class(obj, a_class):
-    """return isintstance of obj and a_class"""
+def is_kind_of_class(obj, a_class):
+    """
+    Write a function that returns True if the object is an instance
+    of, or if the object is an instance of a class that inherited
+    from, the specified class ; otherwise False
+    """
     return isinstance(obj, a_class)
