f26b383790f1abe5972d0db74ea221b336a6cca5
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-08 00:31:18 -0400

    raise exeption custom message for method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class geometry"""
+
+
+class BaseGeometry:
+    """public method raises exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
