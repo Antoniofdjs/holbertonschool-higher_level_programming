397e737409630856cc8bbe657265e32e64727efe
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-07 22:15:15 -0400

    Check if isinstance (inheritance considered)

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Returns true or false if obj is a_class, inheritance considered"""
+
+
+#!/usr/bin/python3
+"""Returns true or false if obj is a_class"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Write a function that returns True if the object is exactly an
+    instance of the specified class ; otherwise False.
+    """
+    return isinstance(obj, a_class)
