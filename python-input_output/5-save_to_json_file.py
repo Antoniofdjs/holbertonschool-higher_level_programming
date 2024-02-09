64acfa266c76281b32a2ddede72ae761313d9517
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-08 22:15:57 -0400

    no utf

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,5 +9,5 @@ def save_to_json_file(my_obj, filename):
     '''
         write json string into file
     '''
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'w') as f:
         json.dumps(my_obj, f)
