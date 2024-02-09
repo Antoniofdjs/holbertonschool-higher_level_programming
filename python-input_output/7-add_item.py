c16d86182e35a51e443da4a9b885e94e41353863
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-08 23:59:51 -0400

    Same code no comments, previous version has # comments

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,15 +8,15 @@ load_json = __import__('6-load_from_json_file').load_from_json_file
 
 my_list = []
 
-# load list from json if file.json exists
+
 try:
     my_list = load_json("add_item.json")[:]
 except FileNotFoundError:
     pass
 
-# get args from the user to add to list
+
 args = sys.argv[1:]
 my_list.extend(args)
 
-# finally save into .json file for future use
+
 save_json(my_list, "add_item.json")
