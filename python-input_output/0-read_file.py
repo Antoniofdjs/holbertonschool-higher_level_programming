d256b9ce96183c27d785f30379c82034f56594d8
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-08 20:20:14 -0400

    comment style again

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-""" This function reads a file """
+'''
+    0-read_file.py
+    Description: Write a function that reads a
+    text file (UTF8) and prints it to stdout
+'''
+
 
 
 def read_file(filename=""):
-    """This is prints """
+    '''
+        Function that reads a text file (UTF8) and prints it to stdout
+    '''
     with open(filename, 'r', encoding='utf-8') as my_file:
         content = my_file.read()
         print("{}".format(content),end='')
