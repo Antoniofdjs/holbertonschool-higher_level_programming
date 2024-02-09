214b688d6ee51ad4ce6ee49adf24efb357c9f624
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-08 22:13:38 -0400

    another form for write the json into the file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,6 +9,5 @@ def save_to_json_file(my_obj, filename):
     '''
         write json string into file
     '''
-    j_object = json.dumps(my_obj)
     with open(filename, 'w', encoding='utf-8') as f:
-        f.write(j_object)
+        json.dumps(my_obj, f)
