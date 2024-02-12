63afa4def1f25a18362fec81c050c9278577c4c2
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-12 16:41:43 -0400

    using name int_validator

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,67 +1,79 @@
 #!/usr/bin/python3
-"""  Class Rectangle inhertis from Base
+
+"""
+Module: rectangle
+Descrip: Rectangle class
+Authors: Lusanco
 """
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    """
-        Rectangle sub class of Base
-    """
+    """Rectangle class inherited from Base"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize Rectangle with id, width, height, x, and y"""
+        self.validator("width", width)
+        self.validator("height", height)
+        self.validator("x", x)
+        self.validator("y", y)
         super().__init__(id)
-        self.int_validator("width", width)
-        self.int_validator("height", height)
-        self.int_validator("x", x)
-        self.int_validator("y", y)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
 
-    def int_validator(self, name, value):
+    def int_validator(self, flag, value):
+        """Validation function for Rectangle"""
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if name == "width" or name == "height":
-            if value <= 0:
-                raise ValueError("{} must be > 0".format(name))
+            raise TypeError("{} must be an integer".format(flag))
         else:
-            if value < 0:
-                raise ValueError("{} must be >= 0".format(name))
+            if (flag == "width" or flag == "height") and value <= 0:
+                raise ValueError("{} must be > 0".format(flag))
+            if (flag == "x" or flag == "y") and value < 0:
+                raise ValueError("{} must be >= 0".format(flag))
 
     @property
     def width(self):
+        """Getter for width attribute."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        self.int_validator("width", value)
+        """Setter for width attribute."""
+        self.validator("width", value)
         self.__width = value
 
     @property
-    def heigth(self):
+    def height(self):
+        """Getter for height attribute."""
         return self.__height
 
-    @heigth.setter
+    @height.setter
     def height(self, value):
-        self.int_validator("height", value)
+        """Setter for height attribute."""
+        self.validator("height", value)
         self.__height = value
 
     @property
     def x(self):
+        """Getter for x attribute."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        self.int_validator("x", value)
+        """Setter for x attribute."""
+        self.validator("x", value)
         self.__x = value
 
     @property
     def y(self):
+        """Getter for y attribute."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        self.int_validator("y", value)
+        """Setter for y attribute."""
+        self.validator("y", value)
         self.__y = value
