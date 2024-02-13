5ed98d051fdc7d52d5e5d86717e9682769c782be
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-12 22:49:31 -0400

    Square sub class

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Square Class
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square, inherits from Rectangle and Base
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """str representation """
+        my_str = "[Square] ({}) ".format(self.id)
+        my_str += "{}/{} ".format(self.x, self.y)
+        my_str += "- {}".format(self.width)
+        # "[Rectangle] (<id>) <x>/<y> - <width>"
+        return my_str
