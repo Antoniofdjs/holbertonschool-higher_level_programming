4210fc6ee271019a5927570937596751b78ca659
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-13 16:06:30 -0400

    to_jsoon_str method added

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,10 +2,12 @@
 """ Base Class
 """
 
+import json
+
 
 class Base:
     """
-    will manage id for other classes
+    will manage id for other classes and json string method
     """
 
     __nb_objects = 0
@@ -18,3 +20,11 @@ class Base:
             Base.__nb_objects += 1
             id = Base.__nb_objects
         self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Turns dictionaries into json string"""
+        if not list_dictionaries or list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
