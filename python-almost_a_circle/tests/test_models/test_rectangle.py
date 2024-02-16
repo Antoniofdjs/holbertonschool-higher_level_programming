36935b235ddbcf12a6ce0d24d8a38e7b7fe6a701
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-16 09:33:51 -0400

    Uni tests, my version

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,56 +7,100 @@ from models.rectangle import Rectangle
 from models.square import Square
 
 
-
 class TestRectangle(unittest.TestCase):
-    """Test cases for Rectangle class"""
+    """Test cases for Rectangle class
+        Rectangle __init__(width,height,x=0,y=0,id=none)
+    """
 
     def setUp(self):
         """Set up for test cases"""
         Base._Base__nb_objects = 0
 
-    def test_rectangle(self):
-        """Test case for non-list arguments"""
+    def test_init_rectangle(self):
+        """test init for Rectangle"""
         r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 1)
         self.assertEqual(r1.width, 10)
         self.assertEqual(r1.height, 2)
-        r3 = Rectangle(1, 2, 3, 4, 5)
-        self.assertEqual(r3.id, 5)
-        self.assertEqual(r3.width, 1)
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
+        self.assertEqual(r1.id, 1)
+
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.width, 10)
         self.assertEqual(r3.height, 2)
-        self.assertEqual(r3.x, 3)
-        self.assertEqual(r3.y, 4)
+        self.assertEqual(r3.x, 0)
+        self.assertEqual(r3.y, 0)
+        self.assertEqual(r3.id, 12)
 
-    def test_rectangle_with_negative_arguments(self):
-        """Test case for negative arguments"""
-        self.assertRaises(ValueError, Rectangle, 10, -2)
-        self.assertRaises(ValueError, Rectangle, -10, 2)
-        self.assertRaises(ValueError, Rectangle, 10, 2, -3)
-        self.assertRaises(ValueError, Rectangle, 10, 2, 3, -4)
+    def test_validator_types(self):
+        """Validate that types must be int, except id"""
+        # Testing creator
+        self.assertRaises(TypeError, Rectangle, "10")
+        self.assertRaises(TypeError, Rectangle, 1, [34])
+        self.assertRaises(TypeError, Rectangle, 1, 2, "4")
+        self.assertRaises(TypeError, Rectangle, 1, 2, 3, "4")
+        # Testing setters
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, 2, 3, 4, 5)
+            r1.width = "-2"
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, 2, 3, 4, 5)
+            r1.height = "0"
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, 2, 3, 4, 5)
+            r1.x = [-1]
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, 2, 3, 4, 5)
+            r1.y = (-1, 4)
 
-    def test_rectangle_with_zero_arguments(self):
-        """Test case for zero arguments"""
-        self.assertRaises(ValueError, Rectangle, 10, 0)
-        self.assertRaises(ValueError, Rectangle, 0, 10)
-
-    def test_rectangle_with_non_list_argument(self):
-        """Test case for non-list arguments"""
-        self.assertRaises(TypeError, Rectangle, 2, "10")
-        self.assertRaises(TypeError, Rectangle, "10", 2)
-        self.assertRaises(TypeError, Rectangle, 10, 2, "5")
-        self.assertRaises(TypeError, Rectangle, 10, 2, 5, "3")
+    def test_validator_values(self):
+        """Validate values (except id), and w,h >0 and x,y >=0"""
+        # Testing constructor
+        self.assertRaises(ValueError, Rectangle, 0, 2)
+        self.assertRaises(ValueError, Rectangle, 1, -3)
+        self.assertRaises(ValueError, Rectangle, 1, 2, -3)
+        self.assertRaises(ValueError, Rectangle, 1, 2, 3, -4)
+        # Testing setters
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(1, 2, 3, 4, 5)
+            r1.width = -2
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(1, 2, 3, 4, 5)
+            r1.height = 0
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(1, 2, 3, 4, 5)
+            r1.x = -1
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(1, 2, 3, 4, 5)
+            r1.y = -1
 
     def test_area(self):
-        """Test case for area method"""
-        r1 = Rectangle(1, 2)
-        self.assertEqual(r1.area(), 2)
+        """test .area, returns area of rectangle"""
+        r10 = Rectangle(3, 2)
+        self.assertEqual(r10.area(), 6)
+        r11 = Rectangle(2, 10)
+        self.assertEqual(r11.area(), 20)
+        r12 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r12.area(), 56)
 
-    def test_str_rectangle(self):
-        """Test case for __str__ method"""
-        r1 = Rectangle(1, 2, 3, 4, 5)
-        self.assertEqual(r1.__str__(), "[Rectangle] (5) 3/4 - 1/2")
+    def test_str_representation(self):
+        """ Represents [Rectangle] (id) x/y - w/h"""
+        r1 = Rectangle(1, 2, 3, 4, 10)
+        self.assertEqual(r1.__str__(), "[Rectangle] (10) 3/4 - 1/2")
+
+    def test_update(self):
+        """ order of udpate is id, width, height, x, y"""
+        r1 = Rectangle(5, 2, 6, 7)
+        r1.update(height=1, id=5)
+        self.assertEqual(r1.__str__(), "[Rectangle] (5) 6/7 - 5/1")
+        r2 = Rectangle(1, 5, 3, 3, 10)
+        r2.update(15, 10, id=5)
+        self.assertEqual(r2.__str__(), "[Rectangle] (15) 3/3 - 10/5")
+        r3 = Rectangle(1, 3)
+        r3.update(id=3, x=4, y=4, width=2, height=3)
+        self.assertEqual(r3.__str__(), "[Rectangle] (3) 4/4 - 2/3")
 
 
 if __name__ == "__main__":
-    unittest.main
+
+    unittest.main()
