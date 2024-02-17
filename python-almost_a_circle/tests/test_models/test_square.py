ff17a321086c5ec7f7c6b77e6058619fdb725f3e
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-17 10:29:49 -0400

    More tests, def update tests

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -68,6 +68,51 @@ class TestRectangle(unittest.TestCase):
             s1 = Square(1)
             s1.y = -2
 
+    def test_area(self):
+        """test .area, returns area of square"""
+        s1 = Square(3)
+        self.assertEqual(s1.area(), 9)
+        s2 = Square(4, 10)
+        self.assertEqual(s2.area(), 16)
+        s3 = Square(2, 7, 6, 10)
+        self.assertEqual(s3.area(), 4)
+
+    def test_str_representation(self):
+        """ Square [Square] (id) x/y - size"""
+        s1 = Square(1, 2, 3, 4)
+        self.assertEqual(s1.__str__(), "[Square] (4) 2/3 - 1")
+
+    def test_update(self):
+        """ order of udpate is [id, width(size), x, y]
+            update(self, *args, **kwargs)
+        """
+        # Testing **kwargs
+        s1 = Square(1, 2, 3, 4)
+        s1.update(id=10)
+        self.assertEqual(s1.__str__(), "[Square] (10) 2/3 - 1")
+        s2 = Square(1, 2, 3, 4)
+        s2.update(id=10, size=10)
+        self.assertEqual(s2.__str__(), "[Square] (10) 2/3 - 10")
+        s3 = Square(1, 2, 3, 4)
+        s3.update(id=10, size=10, x=10)
+        self.assertEqual(s3.__str__(), "[Square] (10) 10/3 - 10")
+        s4 = Square(1, 2, 3, 4)
+        s4.update(id=10, size=10, x=10, y=10)
+        self.assertEqual(s4.__str__(), "[Square] (10) 10/10 - 10")
+        # Testing args
+        s1 = Square(1, 2, 3, 4)
+        s1.update(10, size=10, x=10, y=10)
+        self.assertEqual(s1.__str__(), "[Square] (10) 2/3 - 1")
+        s2 = Square(1, 2, 3, 4)
+        s2.update(10, 10, x=10, y=10)
+        self.assertEqual(s2.__str__(), "[Square] (10) 2/3 - 10")
+        s3 = Square(1, 2, 3, 4)
+        s3.update(10, 10, 10, y=10)
+        self.assertEqual(s3.__str__(), "[Square] (10) 10/3 - 10")
+        s4 = Square(1, 2, 3, 4)
+        s4.update(10, 10, 10, 10)
+        self.assertEqual(s4.__str__(), "[Square] (10) 10/10 - 10")
+
 
 if __name__ == "__main__":
 
