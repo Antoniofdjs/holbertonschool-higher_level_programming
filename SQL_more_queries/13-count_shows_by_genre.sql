6cd2f9894467c725fba4685cb60fc65c146ef9fc
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-03-10 13:22:46 -0400

    right join

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -16,6 +16,6 @@
 
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) as number_of_shows
 FROM tv_genres
-INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+RIGHT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC;
