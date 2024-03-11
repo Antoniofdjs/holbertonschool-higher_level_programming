1b176c17daf55a390667b2f07fa8f0348e07898c
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-03-10 20:05:29 -0400

    changed name to rating for the column of sum

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -5,8 +5,8 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_genres.name , sum(tv_show_ratings.rate) AS rate FROM tv_genres
+SELECT tv_genres.name , sum(tv_show_ratings.rate) AS rating FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.name
-ORDER BY rate DESC;
+ORDER BY rating DESC;
